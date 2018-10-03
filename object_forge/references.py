dc300dd522e841d4938f6295635a77bb4d3ab860
Przemek Kot <przemyslaw.kot@gmail.com>
2018-10-03 16:46:44 +0200

    Create references.py

---
+++ object_forge/references.py
@@ -0,0 +1,47 @@
+# encoding: utf-8
+from textx import metamodel_from_str
+
+
+REFERENCE_SYNTAX = """
+Compd: Call | Ref ;
+Call: "@" Name=ID "." Function=ID "(" (arguments*=Compd[',']) ")";
+Ref: "@" Name=ID;
+"""
+
+
+class Ref:
+    def __init__(self, model, env):
+        self.env = env
+        self.name = model.Name
+
+    def evaluate(self):
+        return self.env.get(self.name)
+
+class Call:
+    def __init__(self, model, env):
+        self.env = env
+        self.object = Ref(model, self.env).evaluate()
+        self.function = model.Function
+        self.arguments = []
+
+        for arg in model.arguments:
+            self.arguments.append(Ref(arg, self.env).evaluate())
+
+    def evaluate(self):
+        return getattr(self.object, self.function)(*self.arguments)
+
+
+class ReferenceExpression:
+    def __init__(self):
+        self.model = metamodel_from_str(REFERENCE_SYNTAX)
+        self.tokens = {
+            'Ref': Ref,
+            'Call': Call,
+        }
+
+    def eeval(self, text, env):
+        print(text)
+        output = self.model.model_from_str(text)
+        print(output)
+        # cls = output.__class__.__name__
+        # return self.tokens.get(cls)(output, env).evaluate()
